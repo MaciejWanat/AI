6f9bcb41d777d975322d9b405722735e23bb4c62
Mieszko <mieszkobor@op.pl>
2018-05-02 17:11:52 +0200

    Add function wich returns neighbours of points

--- Field.py
+++ Field.py
@@ -1,6 +1,5 @@
 import arcade
 
-
 image_path = "app_resources/images/"
 
 class Field(arcade.Sprite):
@@ -23,9 +22,19 @@ class Field(arcade.Sprite):
     def set_parent(self, parent):
         self.parent = parent
 
-    # def get_is_wall_field(self):
-    #     if (x == (width-1) and (y == (width-1))):
-    #         is_wall_field = False
-    #     else:
-    #         is_wall_field = random.randrange(12) == 1
-    #     return is_wall_field
+
+        # for x in range(-70, 71, 70):
+        #     for y in range(-70, 701, 70):
+        #         if x == 0 and y == 0:
+        #             continue
+
+        #         check_x = self.x + x
+        #         check_y = self.y + y
+
+        #         x_index = int(check_x*0.01)
+        #         y_index = int(check_y*0.01)
+
+        #         if (x_index >= 0 and x_index < self.width and y_index >= 0 and y_index < self.height):
+        #             neighbours.insert(len(neighbours), self.grid[y_index][x_index])
+
+        # return neighbours
--- Grid.py
+++ Grid.py
@@ -47,7 +47,13 @@ class Grid:
                     grid[row].append(Field(x,y,"field",True))
 
                 self.items.append(grid[row][column])
-                print(grid[row][column].reachable)
+
+        neightbours = self.get_neighbours(5,5)
+        for (x,y) in neightbours:
+            state = grid[x][y]
+            if state.reachable == False:
+                print(state.isEdible)
+
 
         return grid
 
@@ -57,7 +63,7 @@ class Grid:
                 x =  self.field_width * column + self.field_width // 2
                 y =  self.field_height * row + self.field_height // 2
 
-                background_sprite = arcade.Sprite("app_resources/images/background.png", self.SPRITE_SCALING)
+                background_sprite = arcade.Sprite("app_resources/images/background_lines.png", self.SPRITE_SCALING)
                 background_sprite.width = self.field_width;
                 background_sprite.height = self.field_height;
                 background_sprite.center_x = x
@@ -70,44 +76,23 @@ class Grid:
     def getMushroomsList(self):
         return self.items
 
+    def get_neighbours(self,x,y):
+        # Size of "board"
+        X = 18
+        Y = 9
 
-    # def get_neighbours(self, field):
-    #     neighbours = []
+        neighbors = lambda x, y : [(x2, y2) for x2 in range(x-1, x+2)
+                                       for y2 in range(y-1, y+2)
+                                       if (-1 < x <= X and
+                                           -1 < y <= Y and
+                                           (x != x2 or y != y2) and
+                                           (0 <= x2 <= X) and
+                                           (0 <= y2 <= Y))]
 
-    #     for x in range(-100, 101, 100):
-    #         for y in range(-100, 101, 100):
-    #             if x == 0 and y == 0:
-    #                 continue
-
-    #             check_x = field.x + x
-    #             check_y = field.y + y
-
-    #             x_index = int(check_x*0.01)
-    #             y_index = int(check_y*0.01)
-
-    #             if (x_index >= 0 and x_index < self.width and y_index >= 0 and y_index < self.height):
-
-    #                 neighbours.insert(len(neighbours), self.grid[y_index][x_index])
-
-    #     return neighbours
+        return neighbors(x,y)
 
     # def set_path(self, path):
     #     self.path = path
 
     # def first_and_last(self):
     #     return [self.grid[0][0], self.grid[self.width-1][self.height-1]]
-
-    # def generate_bomb_field_params(self, params_data):
-    #     # 1. Filtrujemy tablice na YES
-    #     params_with_yes = list(filter(lambda x: x[-1] == '1', params_data))
-    #     # 2. Robimy randoma od 0 do tyle ile jest pól YES
-    #     how_many_yeses = len(params_with_yes)
-    #     n = random.randrange(how_many_yeses)
-    #     # 3. Wybeiramy na podstawie randoma paramsy z tablicy
-    #     return params_with_yes[n]
-
-    # def generate_non_bomb_field_params(self, params_data):
-    #     params_with_no = list(filter(lambda x: x[-1] == '0', params_data))
-    #     how_many_no = len(params_with_no)
-    #     n = random.randrange(how_many_no)
-    #     return params_with_no[n]
